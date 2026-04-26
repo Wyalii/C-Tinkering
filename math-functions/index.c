fbda3c1decf48af77e853337ca2540170353161d
Wyalii <gigaurigiorgi2727@email.com>
2026-04-26 20:30:28 +0400

    explored math funcions. linux has problem with libraries on linking stage lol.

---
+++ math-functions/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x = 3;
+    // x = sqrt(x);
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = log(x);
+    // x = sin(x);
+
+    printf("%f", x);
+    return 0;
+}
